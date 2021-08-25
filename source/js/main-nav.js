600227644e5a03f93d04e31c7e871df68c2a5570
Александр <melkov.1994@list.ru>
2021-08-25 15:51:38 +0500

    исправил замечания

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,12 +1,24 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.toggle');
+var button = document.querySelector('.logo-block__button-menu');
 
   navToggle.addEventListener('click', function() {
+    navMain.classList.remove('main-nav--nojs');
     if (navMain.classList.contains('main-nav--closed')) {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--open');
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--open');
+    }
+  });
+
+  button.addEventListener('click', function() {
+    if (button.classList.contains('logo-block__button-menu--closed')) {
+      button.classList.remove('logo-block__button-menu--closed');
+      button.classList.add('logo-block__button-menu--open');
+    } else {
+      button.classList.add('logo-block__button-menu--closed');
+      button.classList.remove('logo-block__button-menu--open');
     }
   });
